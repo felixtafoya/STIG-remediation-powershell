a0343c501fba2407e6b1fe48aabb594a2ac73efd
Felix Tafoya <jamesharden2k20@gmail.com>
2026-03-17 16:32:52 -0700

    Create WN11-CC-000310.ps1

---
+++ STIGS/WN11-CC-000310.ps1
@@ -0,0 +1,45 @@
+<#
+.SYNOPSIS
+This PowerShell script prevents users from changing installation options on Windows 11.
+
+.NOTES
+Author            : Felix Tafoya
+LinkedIn          : linkedin.com/in/felixtafoya/
+GitHub            : github.com/felixtafoya
+Date Created      : 2026-03-17
+Last Modified     : 2026-03-17
+Version           : 1.0
+CVEs              : N/A
+Plugin IDs        : N/A
+STIG-ID           : WN11-CC-000310
+
+.TESTED ON
+Date(s) Tested    :
+Tested By         :
+Systems Tested    :
+PowerShell Ver.   :
+
+.USAGE
+Run this script in PowerShell as Administrator.
+
+Example syntax:
+PS C:\> .\WN11-CC-000310.ps1
+#>
+
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Installer"
+$valueName = "EnableUserControl"
+$valueData = 0
+
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force | Out-Null
+}
+
+New-ItemProperty -Path $registryPath -Name $valueName -Value $valueData -PropertyType DWord -Force | Out-Null
+
+$result = Get-ItemProperty -Path $registryPath -Name $valueName -ErrorAction SilentlyContinue
+
+if ($result.$valueName -eq $valueData) {
+    Write-Host "SUCCESS"
+} else {
+    Write-Host "FAIL"
+}
